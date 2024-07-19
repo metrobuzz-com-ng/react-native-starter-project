7014418d2b2abc9172a0096aad2209178a6f125d
Chima Nwachukwu <tylerdgenius@Chimas-MBP.(none)>
2024-07-19 12:52:42 +0100

    feat: added dotenv support

---
+++ env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const WELCOME_TEXT: string;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {WELCOME_TEXT} from '@env';
+
 export default () => {
-  return <></>;
+  return (
+    <View style={styles.center}>
+      <Text>{WELCOME_TEXT}</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
